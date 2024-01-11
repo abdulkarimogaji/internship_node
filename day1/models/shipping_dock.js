37b92b5f9b2957457c913980ce8c4a9796738d59
abdulkarim <abdulkarimogaji001@gmail.com>
2024-01-11 15:49:21 +0100

    day_1: setup models

---
+++ day1/models/shipping_dock.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+  const shipping_dock = sequelize.define(
+    "shipping_dock",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: DataTypes.STRING,
+      status: DataTypes.TINYINT,
+      created_at: DataTypes.DATEONLY,
+      updated_at: DataTypes.DATE,
+    },
+    {
+      timestamps: true,
+      freezeTableName: true,
+      tableName: "shipping_dock",
+    },
+    {
+      underscoredAll: false,
+      underscored: false,
+    }
+  );
+
+  return shipping_dock;
+};
