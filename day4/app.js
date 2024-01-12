895014fbfe8ff71a0653162616d0c026bbb22603
abdulkarim <abdulkarimogaji001@gmail.com>
2024-01-12 16:46:01 +0100

    day_4: CRUD apis for email

--- day4/app.js
+++ day4/app.js
@@ -6,6 +6,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var emailRouter = require("./routes/email");
 
 const db = require("./models");
 var cors = require("cors");
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/api/v1/user", usersRouter);
+app.use("/api/v1/email", emailRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ day4/routes/email.js
@@ -0,0 +1,90 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET all. */
+router.get("/", async (req, res, next) => {
+  try {
+    const db = req.app.get("db");
+    const result = await db.email.findAll();
+    res.status(200).json({ error: false, list: result });
+  } catch (err) {
+    console.log("err", err);
+    res.status(500).json({ error: true, message: "Something went wrong" });
+  }
+});
+
+/* GET one. */
+router.get("/:id", async (req, res, next) => {
+  try {
+    const db = req.app.get("db");
+    const result = await db.email.findByPk(req.params.id);
+
+    if (!result) {
+      return res.status(404).json({ error: true, message: "Not found" });
+    }
+    res.status(200).json({ error: false, model: result });
+  } catch (err) {
+    console.log("err", err);
+    res.status(500).json({ error: true, message: "Something went wrong" });
+  }
+});
+
+/* ADD one. */
+router.post("/", async (req, res, next) => {
+  try {
+    const db = req.app.get("db");
+    const result = await db.email.create({
+      slug: req.body.slug,
+      subject: req.body.subject,
+      body: req.body.body,
+      status: req.body.status,
+    });
+
+    res.status(201).json({ error: false, model: result });
+  } catch (err) {
+    console.log("err", err);
+    res.status(500).json({ error: true, message: "Something went wrong" });
+  }
+});
+
+/* update one. */
+router.put("/:id", async (req, res, next) => {
+  try {
+    const db = req.app.get("db");
+
+    await db.email.update(
+      {
+        slug: req.body.slug,
+        subject: req.body.subject,
+        body: req.body.body,
+        status: req.body.status,
+      },
+      { where: { id: req.params.id } }
+    );
+
+    res.status(200).json({ error: false, message: "Update successful" });
+  } catch (err) {
+    console.log("err", err);
+    res.status(500).json({ error: true, message: "Something went wrong" });
+  }
+});
+
+/* delete one. */
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const db = req.app.get("db");
+
+    await db.email.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    res.status(200).json({ error: false, message: "Delete successful" });
+  } catch (err) {
+    console.log("err", err);
+    res.status(500).json({ error: true, message: "Something went wrong" });
+  }
+});
+
+module.exports = router;
