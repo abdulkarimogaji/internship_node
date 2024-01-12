03a288c1518b39f54153903d03a35af224bfdbf6
abdulkarim <abdulkarimogaji001@gmail.com>
2024-01-12 16:20:47 +0100

    day_3: crud APIs for variables

--- day3/app.js
+++ day3/app.js
@@ -7,6 +7,7 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var rulesRouter = require("./routes/rules");
+var variablesRouter = require("./routes/variables");
 
 const db = require("./models");
 var cors = require("cors");
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/api/v1/rules", rulesRouter);
+app.use("/api/v1/variables", variablesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
