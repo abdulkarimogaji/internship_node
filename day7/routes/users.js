68edd4be28f761b4e181a1da30f50d70b36e6ec0
abdulkarim <abdulkarimogaji001@gmail.com>
2024-01-15 23:14:04 +0100

    day_7: crud apis for user table

--- day7/routes/users.js
+++ day7/routes/users.js
@@ -1,9 +1,84 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET all. */
+router.get("/", async (req, res, next) => {
+  try {
+    const db = req.app.get("db");
+    const result = await db.user.findAll();
+    res.status(200).json({ error: false, list: result });
+  } catch (err) {
+    console.log("err", err);
+    res.status(500).json({ error: true, message: "Something went wrong" });
+  }
+});
+
+/* GET one. */
+router.get("/:id", async (req, res, next) => {
+  try {
+    const db = req.app.get("db");
+    const result = await db.user.findByPk(req.params.id);
+
+    if (!result) {
+      return res.status(404).json({ error: true, message: "Not found" });
+    }
+    res.status(200).json({ error: false, model: result });
+  } catch (err) {
+    console.log("err", err);
+    res.status(500).json({ error: true, message: "Something went wrong" });
+  }
+});
+
+/* ADD one. */
+router.post("/", async (req, res, next) => {
+  try {
+    const db = req.app.get("db");
+    const result = await db.user.create({
+      name: req.body.name,
+    });
+
+    res.status(201).json({ error: false, model: result });
+  } catch (err) {
+    console.log("err", err);
+    res.status(500).json({ error: true, message: "Something went wrong" });
+  }
+});
+
+/* update one. */
+router.put("/:id", async (req, res, next) => {
+  try {
+    const db = req.app.get("db");
+
+    await db.user.update(
+      {
+        name: req.body.name,
+      },
+      { where: { id: req.params.id } }
+    );
+
+    res.status(200).json({ error: false, message: "Update successful" });
+  } catch (err) {
+    console.log("err", err);
+    res.status(500).json({ error: true, message: "Something went wrong" });
+  }
+});
+
+/* delete one. */
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const db = req.app.get("db");
+
+    await db.user.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    res.status(200).json({ error: false, message: "Delete successful" });
+  } catch (err) {
+    console.log("err", err);
+    res.status(500).json({ error: true, message: "Something went wrong" });
+  }
 });
 
 module.exports = router;
