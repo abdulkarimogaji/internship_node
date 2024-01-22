57c1a619684381cb236855873a57160ce182bb95
abdulkarim <abdulkarimogaji001@gmail.com>
2024-01-22 05:15:48 +0100

    day_13: products list page

--- day13/routes/index.js
+++ day13/routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", async (req, res, next) => {
+  res.render("index", { title: "Express" });
+});
+
+/* GET products page. */
+router.get("/products", async (req, res, next) => {
+  try {
+    const db = req.app.get("db");
+
+    const products = await db.product.findAll();
+    res.render("products", { title: "Products", products });
+  } catch (err) {
+    res.render("error", {
+      message: "An error occurred",
+      error: { status: 500, stack: err },
+    });
+  }
 });
 
 module.exports = router;
